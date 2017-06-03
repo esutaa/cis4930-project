8c866159f8fb77c546f17d3c6672bbf9667160cc
jse13 <jsebastianengel@gmail.com>
2017-06-03 12:22:16 -0400

    Boilerplate for a basic game loop

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+import pygame as pg
+
+pg.init()
+
+gameDisplay = pg.display.set_mode((800, 600))
+pg.display.set_caption('Test Caption')
+
+clock = pg.time.Clock()
+
+
+crashed = False
+
+# This is the main game loop
+while not crashed:
+
+  for event in pg.event.get():
+    if event.type == pg.QUIT:
+      crashed = True
+    print(event)
+
+  pg.display.update()
+  clock.tick(60)
+
+
+pg.quit()
+quit()
