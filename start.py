ce9ab65ae507a58fcaffbf7e5e9630c64b621e2c
Julian Engel <me@julianengel.com>
2017-06-14 09:01:07 -0400

    Added code for generating resources and entering game loop

--- start.py
+++ start.py
@@ -4,7 +4,9 @@ Main module of the game.
 """
 import pygame
 import constants as C
+from resources import Resources
 from main_menu import main_menu
+from game_loop import game_loop
 
 pygame.init()
 pygame.display.set_caption(C.GAME_NAME)
@@ -12,3 +14,5 @@ pygame.display.set_caption(C.GAME_NAME)
 
 if __name__ == '__main__':
     main_menu()
+    RES = Resources()
+    game_loop(RES)
