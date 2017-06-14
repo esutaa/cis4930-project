12da5f07e1c257194e7b9057caa1ac57ca6cf3f0
Julian Engel <me@julianengel.com>
2017-06-14 09:03:09 -0400

    boilerplate for a class that manages the game loop

---
+++ game_loop.py
@@ -0,0 +1,13 @@
+"""
+This module controls the main control flow of the game.
+"""
+import pygame
+import constants as C
+from resources import Resources
+
+
+def game_loop(RES):
+    """
+    Manager function that's going to be called from the start.py module.
+    """
+    print("foo")
