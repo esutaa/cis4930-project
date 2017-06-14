0733560e63d743143ed9456a42dd0bb861c43a89
Julian Engel <me@julianengel.com>
2017-06-14 09:02:51 -0400

    boilerplate for a class to manage game resources

---
+++ resources.py
@@ -0,0 +1,14 @@
+"""
+Module to contain the Resource class.
+NOTE: this is a Python 3 class! Please be aware that there may be important
+differences between Python 2 and 3 classes!
+"""
+class Resources:
+    """
+    The intention of this class is to generate, contain, and facilitate
+    interaction with resources that are dynamically loaded into the game before
+    the main game loop begins.
+    """
+    def __init__(self):
+        print("Placeholder text, delete me!")
+
