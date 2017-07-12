b524cc7e32415153ec49c09ad8fc8c0e22621f85
Julian Engel <me@julianengel.com>
2017-07-12 14:05:10 -0400

    better collision detection, but still jittery

--- testFiles/testCollision.py
+++ testFiles/testCollision.py
@@ -35,30 +35,65 @@ class PlayerCharacter(pygame.sprite.Sprite):
         self.rect = self.sprite.get_rect()
 
         self.prediction_rect = self.rect.inflate(32, 32)
+        self.alert = False
+        self.movement_allowance = {'Up' : -1, 'Down' : -1, 'Left' : -1, 'Right' : -1}
         self.collisions = {'Top': False, 'Left': False, 'Bottom' : False, 'Right' : False}
 
         # Bitmask for collision
         self.mask = pygame.mask.from_surface(PlayerCharacter.image)
 
     def move(self, direction):
+        move_amt = 0
         if direction is "right":
             if not self.collisions['Right']:
-                player.playerx += round(PlayerCharacter.dx * seconds)
+                move_amt = round(PlayerCharacter.dx * seconds)
+                if self.movement_allowance['Right'] != 1000:
+                    if move_amt > self.movement_allowance['Right']:
+                        move_amt = self.movement_allowance['Right']
+
+                    if self.movement_allowance['Right'] == 0 and not self.collisions['Right']:
+                        move_amt = round(PlayerCharacter.dx * seconds) 
+
+                player.playerx += move_amt
             else:
                 print("Blocked on {}!".format(direction))
         elif direction is "left":
             if not self.collisions['Left']:
-                player.playerx -= round(PlayerCharacter.dx * seconds)
+                move_amt = round(PlayerCharacter.dx * seconds)
+                if self.movement_allowance['Left'] != 1000:
+                    if move_amt > self.movement_allowance['Left']:
+                        move_amt = self.movement_allowance['Left']
+
+                    if self.movement_allowance['Left'] == 0 and not self.collisions['Left']:
+                        move_amt = round(PlayerCharacter.dx * seconds) 
+
+                player.playerx -= move_amt
             else:
                 print("Blocked on {}!".format(direction))
         elif direction is "up":
             if not self.collisions['Top']:
-                player.playery -= round(PlayerCharacter.dy * seconds)
+                move_amt = round(PlayerCharacter.dy * seconds)
+                if self.movement_allowance['Up'] != 1000:
+                    if move_amt > self.movement_allowance['Up']:
+                        move_amt = self.movement_allowance['Up']
+
+                    if self.movement_allowance['Up'] == 0 and not self.collisions['Bottom']:
+                        move_amt = round(PlayerCharacter.dy * seconds) 
+
+                player.playery -= move_amt
             else:
                 print("Blocked on {}!".format(direction))
         elif direction is "down":
             if not self.collisions['Bottom']:
-                player.playery += round(PlayerCharacter.dy * seconds)
+                move_amt = round(PlayerCharacter.dy * seconds)
+                if self.movement_allowance['Down'] != 1000:
+                    if move_amt > self.movement_allowance['Down']:
+                        move_amt = self.movement_allowance['Down']
+
+                    if self.movement_allowance['Down'] == 0 and not self.collisions['Bottom']:
+                        move_amt = round(PlayerCharacter.dy * seconds) 
+
+                player.playery += move_amt
             else:
                 print("Blocked on {}!".format(direction))
 
@@ -68,48 +103,93 @@ class PlayerCharacter(pygame.sprite.Sprite):
         for wall in blockgroup:
             if self.prediction_rect.colliderect(wall.rect):
                 wall.alert = True
+                self.alert = True
+
+            if wall.rect.collidepoint(self.prediction_rect.topleft) or \
+            wall.rect.collidepoint(self.prediction_rect.midtop) or \
+            wall.rect.collidepoint(self.prediction_rect.topright):
+                self.movement_allowance['Up'] = self.rect.midtop[1] - wall.rect.midbottom[1] 
+                if self.movement_allowance['Up'] < 0:
+                    self.movement_allowance['Up'] = 0
+                #print("Movement allowance up is {} pixels".format(self.movement_allowance['Up']))
+            else:
+                self.movement_allowance['Up'] = 1000
+
+            if wall.rect.collidepoint(self.prediction_rect.topleft) or \
+            wall.rect.collidepoint(self.prediction_rect.midleft) or \
+            wall.rect.collidepoint(self.prediction_rect.bottomleft):
+                self.movement_allowance['Left'] = self.rect.midleft[0] - wall.rect.midright[0]
+                if self.movement_allowance['Left'] < 0:
+                    self.movement_allowance['Left'] = 0
+                #print("Movement allowance left is {} pixels".format(self.movement_allowance['Left']))
+            else:
+                self.movement_allowance['Left'] = 1000
+
+            if wall.rect.collidepoint(self.prediction_rect.bottomleft) or \
+            wall.rect.collidepoint(self.prediction_rect.midbottom) or \
+            wall.rect.collidepoint(self.prediction_rect.bottomright):
+                self.movement_allowance['Down'] = wall.rect.midtop[1] - self.rect.midbottom[1]
+                if self.movement_allowance['Down'] < 0:
+                    self.movement_allowance['Down'] = 0
+                #print("Movement allowance down is {} pixels".format(self.movement_allowance['Down']))
+            else:
+                self.movement_allowance['Down'] = 1000
+
+            if wall.rect.collidepoint(self.prediction_rect.bottomright) or \
+            wall.rect.collidepoint(self.prediction_rect.midright) or \
+            wall.rect.collidepoint(self.prediction_rect.topright):
+                self.movement_allowance['Right'] = wall.rect.midleft[0] - self.rect.midright[0]
+                if self.movement_allowance['Right'] < 0:
+                    self.movement_allowance['Right'] = 0
+                #print("Movement allowance right is {} pixels".format(self.movement_allowance['Right']))
+            else:
+                self.movement_allowance['Right'] = 1000
 
         self.collisions['Top'] = False
         self.collisions['Left'] = False
         self.collisions['Bottom'] = False
         self.collisions['Right'] = False
 
-        for wall in pygame.sprite.spritecollide(self, blockgroup, False, pygame.sprite.collide_mask):
+        for wall in pygame.sprite.spritecollide(self, blockgroup, False, pygame.sprite.collide_rect):
             wall.blocking = True
             self.blocked = True
 
-            if wall.rect.collidepoint(self.rect.topleft):
-                self.collisions['Top'] = True
-            if wall.rect.collidepoint(self.rect.midtop):
-                self.collisions['Top'] = True
-            if wall.rect.collidepoint(self.rect.topright):
+            if wall.rect.collidepoint(self.rect.topleft) or \
+            wall.rect.collidepoint(self.rect.midtop) or \
+            wall.rect.collidepoint(self.rect.topright):
                 self.collisions['Top'] = True
 
-            if wall.rect.collidepoint(self.rect.topleft):
-                self.collisions['Left'] = True
-            if wall.rect.collidepoint(self.rect.midleft):
-                self.collisions['Left'] = True
-            if wall.rect.collidepoint(self.rect.bottomleft):
+            if wall.rect.collidepoint(self.rect.topleft) or \
+            wall.rect.collidepoint(self.rect.midleft) or \
+            wall.rect.collidepoint(self.rect.bottomleft):
                 self.collisions['Left'] = True
 
-            if wall.rect.collidepoint(self.rect.bottomleft):
-                self.collisions['Bottom'] = True
-            if wall.rect.collidepoint(self.rect.midbottom):
-                self.collisions['Bottom'] = True
-            if wall.rect.collidepoint(self.rect.bottomright):
+            if wall.rect.collidepoint(self.rect.bottomleft) or \
+            wall.rect.collidepoint(self.rect.midbottom) or \
+            wall.rect.collidepoint(self.rect.bottomright):
                 self.collisions['Bottom'] = True
 
-            if wall.rect.collidepoint(self.rect.bottomright):
+            if wall.rect.collidepoint(self.rect.bottomright) or \
+            wall.rect.collidepoint(self.rect.midright) or \
+            wall.rect.collidepoint(self.rect.topright):
                 self.collisions['Right'] = True
-            if wall.rect.collidepoint(self.rect.midright):
-                self.collisions['Right'] = True
-            if wall.rect.collidepoint(self.rect.topright):
-                self.collisions['Right'] = True
+
+            # If the sprite is inside of another one, bump it out
+            if self.collisions['Top'] and self.rect.midtop[1] < wall.rect.midbottom[1]:
+                self.playery += wall.rect.midbottom[1] - self.rect.midtop[1]
+            if self.collisions['Left'] and self.rect.midleft[0] < wall.rect.midright[0]:
+                self.playerx += wall.rect.midright[0] - self.rect.midleft[0]
+            if self.collisions['Bottom'] and self.rect.midbottom[1] > wall.rect.midtop[1]:
+                self.playery -= self.rect.midbottom[1] - wall.rect.midtop[1]
+            if self.collisions['Right'] and self.rect.midright[0] > wall.rect.midleft[0]:
+                self.playerx -= self.rect.midright[0] - wall.rect.midleft[0]
 
 
         self.rect.center = (self.playerx, self.playery)
         self.prediction_rect.center = (self.playerx, self.playery)
         pygame.draw.rect(screen, (255,255,255), self.prediction_rect, 1)
+
+        self.alert = False
 
 PlayerCharacter.groups = allgroup, playergroup, collisiongroup
 
@@ -186,6 +266,7 @@ while loop:
         allgroup.clear(screen, background)
         allgroup.update(seconds)
         allgroup.draw(screen)
+
 
         pygame.display.flip()
 
